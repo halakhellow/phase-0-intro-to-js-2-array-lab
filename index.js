a48c86caffdf33cdf162b4b9386441f8a382097e
Hala Alkhellow <halakhellow1996@gmail.com>
2022-04-08 22:20:21 +0300

    define destructivelyRemoveLastCat & destructivelyRemoveFirstCat functions

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ let cats = ["Milo", "Otis", "Garfield"];
 
 let destructivelyAppendCat = () => cats.push("Ralph");
 let destructivelyPrependCat = () => cats.unshift("Bob");
+
+let destructivelyRemoveLastCat = () => cats.pop();
+let destructivelyRemoveFirstCat = () => cats.shift();
