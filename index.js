ad7b2725e25c05fd82cfe203939354d4390772b5
Hala Alkhellow <halakhellow1996@gmail.com>
2022-04-08 22:21:18 +0300

    add removeLastCat & removeFirstCat functions

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ let destructivelyRemoveFirstCat = () => cats.shift();
 
 let appendCat = () => { return [...cats, "Broom"] }
 let prependCat = () => { return ["Arnold", ...cats] }
+
+let removeLastCat = () => { return cats.slice(0, 2) }
+let removeFirstCat = () => { return cats.slice(1) }
