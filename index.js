ebb41e2e8fbb8a6442c9c8d62b312dbb2b5680f9
Hala Alkhellow <halakhellow1996@gmail.com>
2022-04-15 17:11:51 +0300

    change adding or deleting new cats in the functions to be dynamic

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let cats = ["Milo", "Otis", "Garfield"];
 
-let destructivelyAppendCat = () => cats.push("Ralph");
-let destructivelyPrependCat = () => cats.unshift("Bob");
+let destructivelyAppendCat = (catName) => cats.push(catName);
+let destructivelyPrependCat = (catName) => cats.unshift(catName);
 
 let destructivelyRemoveLastCat = () => cats.pop();
 let destructivelyRemoveFirstCat = () => cats.shift();
 
-let appendCat = () => { return [...cats, "Broom"] }
-let prependCat = () => { return ["Arnold", ...cats] }
+let appendCat = (catName) => { return [...cats, catName] }
+let prependCat = (catName) => { return [catName, ...cats] }
 
 let removeLastCat = () => { return cats.slice(0, 2) }
 let removeFirstCat = () => { return cats.slice(1) }
